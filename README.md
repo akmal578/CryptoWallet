ac0eefa24a8ed2a8665f5e25b471aa3f82dd2371
Akmal Adnan <akmal.adnan@ymail.com>
2022-06-05 22:28:48 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+### CryptoWallet UI test
---
+++ src/components/TabIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+import {FONTS, COLORS} from '../constants';
+
+const TabIcon = () => {
+  return (
+    <View>
+      <Text>Tab</Text>
+    </View>
+  );
+};
+
+export default TabIcon;
